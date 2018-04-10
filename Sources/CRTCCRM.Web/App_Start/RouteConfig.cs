7dbaa83a1b150bf4db12c7588f2c503c4dd0c126
Roman Makarevich <remmybump@gmail.com>
2018-04-10 13:16:17 +0300

    Added approved version of simple frontend.

--- Sources/CRTCCRM.Web/App_Start/RouteConfig.cs
+++ Sources/CRTCCRM.Web/App_Start/RouteConfig.cs
@@ -13,6 +13,35 @@ namespace CRTCCRM.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("index", "", new {
+                controller = "Home",
+                action = "Index"
+            });
+            routes.MapRoute("login", "login", new {
+                controller = "Home",
+                action = "Login"
+            });
+            routes.MapRoute("contacts", "contacts", new {
+                controller = "Home",
+                action = "Contacts"
+            });
+            routes.MapRoute("createLeads", "createLeads", new {
+                controller = "Home",
+                action = "CreateLeads"
+            });
+            routes.MapRoute("editLead", "editLead", new {
+                controller = "Home",
+                action = "EditLead"
+            });
+            routes.MapRoute("leads", "leads", new {
+                controller = "Home",
+                action = "Leads"
+            });
+            routes.MapRoute("viewLead", "viewLead", new {
+                controller = "Home",
+                action = "ViewLead"
+            });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Sources/CRTCCRM.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace CRTCCRM.Web.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Login() {
+            return View();
+        }
+
+        public ActionResult Contacts() {
+            return View();
+        }
+
+        public ActionResult CreateLeads() {
+            return View();
+        }
+
+        public ActionResult EditLead() {
+            return View();
+        }
+
+        public ActionResult Leads() {
+            return View();
+        }
+
+        public ActionResult ViewLead() {
+            return View();
+        }
+    }
+}
